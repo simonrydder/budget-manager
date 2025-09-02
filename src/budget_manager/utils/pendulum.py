456ed4815612a8fa4401af2b8e2f605fa89696f3
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-09-02 11:14:24 +0200

    feat: added function to jump between months across different years.

--- src/budget_manager/utils/pendulum.py
+++ src/budget_manager/utils/pendulum.py
@@ -1,5 +1,7 @@
 import pendulum
 
+from budget_manager.types import Month
+
 
 def next_first(date: pendulum.Date) -> pendulum.Date:
     first_this_month = date.start_of("month")
@@ -7,3 +9,9 @@ def next_first(date: pendulum.Date) -> pendulum.Date:
         return date
     else:
         return first_this_month.add(months=1)
+
+
+def month_shifter(month: Month, year: int, shift: int) -> tuple[Month, int]:
+    date = pendulum.Date(year, month, 1) + pendulum.Duration(months=shift)
+
+    return (date.month, date.year)
