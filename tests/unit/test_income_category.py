9dda490bb188904ea8a603bc780e3e84119ea797
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-09-11 14:26:00 +0200

    fix: incorrect test

--- tests/unit/test_income_category.py
+++ tests/unit/test_income_category.py
@@ -86,6 +86,7 @@ def test_that_expected_income_in_may_is_500(salary: IncomeCategory):
 
 def test_that_expected_income_with_two_income_plans_sum(salary: IncomeCategory):
     salary.incomes.append(Record(500, Date(2025, 3, 31)))
+    salary.income_plans.append(HistoricalIncomePlan())
 
     assert salary.expected_income(5, 2025) == 1000
 
